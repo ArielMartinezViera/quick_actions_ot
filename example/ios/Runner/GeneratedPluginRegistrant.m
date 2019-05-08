0e278285d4fc837d4f0f4a4508386e354c6eb299
xia-weiyang <gw@jiushig.com>
2019-05-08 19:01:49 +0800

    init

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <quick_actions_ot/QuickActionsPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTQuickActionsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTQuickActionsPlugin"]];
+}
+
+@end
